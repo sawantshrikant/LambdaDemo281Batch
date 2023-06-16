d5d624b4d46d8f531f4e44a52abe0f855d0fb46e
Shrikant <sawantshrikant07@gmail.com>
2023-06-16 11:48:58 +0530

    [Shrikant] add:PersonDatAdd

---
+++ Person.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LambdaDemo281Batch
+{
+    public class Person
+    {
+        public int SSN {  get; set; }
+        public String Name { get; set; }
+        public string Address { get; set; }
+        public int Age { get; set; }
+
+        public override string ToString()
+        {
+            return $"serial no:{SSN} name:{Name} address:{Address} age:{Age}";
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -5,6 +5,30 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome To Lambda Demo Problem!");
+
+            List<Person> list = new List<Person>()
+            {
+                new Person() {SSN =1,Name="Shrikant", Address="abc1", Age=25},
+                new Person() {SSN =2,Name="Ram", Address="DEF2", Age=30},
+                new Person() {SSN =3,Name="Sham", Address="ghi3", Age=35},
+                new Person() {SSN =4,Name="Sammer", Address="ghi3", Age=45},
+                new Person() {SSN =5,Name="Sagar", Address="poi3", Age=65},
+                new Person() {SSN =6,Name="Komal", Address="poi3", Age=18},
+                new Person() {SSN =7,Name="om", Address="poi3", Age=63},
+
+
+            };
+            DisplayPersonData(list);
         }
+
+        public static void DisplayPersonData(List<Person> list)
+        {
+            foreach (Person person in list)
+            {
+                Console.WriteLine(person);
+            }
+        }
+
+       
     }
 }
